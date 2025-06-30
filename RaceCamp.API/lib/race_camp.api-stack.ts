0e162e3a7e7dba486e836ccdbc4fa4c2fa31539e
NicoleNG72 <nicolengentil@gmail.com>
2025-06-29 20:30:04 -0400

    Deploy Site with S3 & CloudFront

--- RaceCamp.API/lib/race_camp.api-stack.ts
+++ RaceCamp.API/lib/race_camp.api-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
+import { CfnOutput } from 'aws-cdk-lib';
+
+import * as path from 'path';
 
 export class RaceCampApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //new CfnOutput(this, 'Site', { value: 'https://' + siteDomain });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RaceCampApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hostBucket = new s3.Bucket(this, 'HostBucket', {
+      bucketName: `host-bucket.s3.${process.env.AWS_REGION}.amazonaws.com`,
+      versioned: true, 
+      websiteIndexDocument:'index.html',
+      // websiteErrorDocument: 'error.html',
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, 
+      autoDeleteObjects: true,
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../RaceCamp.Web/dist'))],
+      destinationBucket: hostBucket,
+    });
+
+    const cfDisctribution = new cloudfront.Distribution(this, 'CloudFrontDisctribution', {
+      defaultBehavior: {
+        origin: cloudfront_origins.S3BucketOrigin.withOriginAccessControl(hostBucket)
+      }
+    })
+
+    new CfnOutput(this, 'DomainName', { value:  cfDisctribution.distributionDomainName + "/index.html"});
   }
 }
